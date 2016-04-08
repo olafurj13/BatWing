643e737f378cc548eb7cbc5cfa3a0c33220f6961
Sveinn Henrik Kristinsson <Svenni@Sveinns-Air.lan>
2016-04-08 15:56:34 +0000

    Sound on

--- app/scripts/sound.js
+++ app/scripts/sound.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var muted = true;
+
+function mute() {
+	var backgroundMusic = document.getElementById('backgroundMusic');
+	var muter = document.getElementById('muter');
+	if (muted == true){
+		backgroundMusic.muted = false;
+		muter.src = 'images/unmute.png';
+		muted = false;
+	}
+	else {
+	    backgroundMusic.muted = true;
+	    muter.src = 'images/mute2.png';
+	    muted = true;
+	}
+}
+mute();
